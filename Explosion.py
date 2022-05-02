3159ef6fda5d55c52e965829b7bd33febc8c247b
Simeon <simeonpopov94@gmail.com>
2022-05-02 18:18:52 +0300

    # 15 Eplosion BUG fix

--- Explosion.py
+++ Explosion.py
@@ -9,11 +9,8 @@ class Explosion(pg.sprite.Sprite):
         self.image = pg.image.load("data/Explosion/1.png").convert_alpha()
         self.image = pg.transform.scale(self.image, (Explosion.EXPLOSION_DIMENSIONS[0], Explosion.EXPLOSION_DIMENSIONS[1]))
         self.rect = self.image.get_rect()
-
-        # IDK why this offset is that big but it is
-        self.rect.x = object.rect.x-80
-        self.rect.y = object.rect.y-50
-        self.frame = 1
+        self.rect.center = object.rect.center
+        self.frame = 26
 
     def update(self) -> None:
         self.image = pg.image.load("data/Explosion/{}.png".format(self.frame)).convert_alpha()
