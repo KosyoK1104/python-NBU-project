cd32b83a02f7e1437816da88b15708b539af022a
Simeon <simeonpopov94@gmail.com>
2022-05-07 01:15:27 +0300

    #23 Spawning weight is created inside EnemyFactory - Mobs are spawning via chance based on how close the mob level is to the current Game level - The chance algorithm can be made faster if we replace weights with-> cum_weights

--- EnemyFactory.py
+++ EnemyFactory.py
@@ -17,7 +17,20 @@ class EnemyFactory:
         alien_data = json.load(open("data/alien.json"))
         # enemyType = 1
         # level = 1
-        alien_level = random.randint(1, level)
+        # alien_level = random.randint(1, level)
+        if level > 1:
+            alien_level_dict = dict()
+            # loop that generates dict with key = level and value = weighted chance
+            for i in range(1, level+1):
+                alien_level_dict[i] = int(i * 10)
+            # keys represent different levels, values represent chance of spawning
+            # print(list(alien_level_dict.values()))
+            # print(list(alien_level_dict.keys()))
+            alien_level = random.choices(list(alien_level_dict.keys()), weights=list(alien_level_dict.values()), k=1)[0]
+            # print(alien_level)
+        else:
+            alien_level = 1
+
 
         if enemyType == 1:
             return Alien(random.choice(alien_data['data'][str(alien_level)])['sprite'], alien_level)
